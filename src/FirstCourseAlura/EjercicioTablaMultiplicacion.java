48312e842c78269ed516fef5a2d476c6f451293d
Jorge Mendez <jorgemendez3421@gmail.com>
2023-02-23 07:47:25 -0800

    Ejercicio utilizando for

---
+++ src/FirstCourseAlura/EjercicioTablaMultiplicacion.java
@@ -0,0 +1,18 @@
+package FirstCourseAlura;
+
+public class EjercicioTablaMultiplicacion {
+	
+	public static void main(String[] args) {
+		
+		for (int contador = 2; contador <= 2 ; contador++) {
+			for (int multiplicacion = 1; multiplicacion <= 10; multiplicacion ++) {
+				System.out.print(" ");
+				System.out.println(contador + " X "+ multiplicacion 
+						+ " : " + contador * multiplicacion);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
